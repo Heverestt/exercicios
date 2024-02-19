2eae382bb17e0245c9770067566be03209e1fb6c
Heverestt <159032916+Heverestt@users.noreply.github.com>
2024-02-19 10:49:51 -0300

    Create Ex3.js

---
+++ Exercicio01/Ex3.js
@@ -0,0 +1,6 @@
+et actualNum = prompt("Digite o número de qual você quer saber a tabuada");
+let lastNum = 10;
+
+for(let numAtual = 1; (numAtual<=lastNum); numAtual++){
+  console.log(numAtual*actualNum);
+};
